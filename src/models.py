ac9d63341c0d8f20aa5f8f4c04415c6ceacdb59f
ManulPrian <156845683+ManulPrian@users.noreply.github.com>
2024-05-09 14:48:55 +0000

    la segunda

--- src/models.py
+++ src/models.py
@@ -1,48 +1,65 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+
 Base = declarative_base()
-class Users(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key = True)
-    username = Column(String(20), nullable=False)
-    name = Column(String(20), nullable=False)
-    last_name = Column(String(20), nullable=False)
-    password = Column(String(20), nullable=False)
-    email = Column(String(30), nullable=False)
-class Followers(Base):
-    __tablename__ = 'followers'
-    user_from_id = Column(Integer, primary_key=True)
-    user_to_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(Users)
-class Comment(Base):
-    __tablename__ = 'comment'
-    id = Column(Integer, primary_key = True)
-    comment_Text = Column(String(200), nullable=False)
-    author_id = Column(Integer,ForeignKey('users.id'))
-    post_id = Column(Integer,ForeignKey('post.id'))
-    users = relationship(Users)
-class Post(Base):
-    __tablename__ ='post'
-    id = Column(Integer, primary_key = True)
-    user_id = Column(Integer,ForeignKey('users.id'))
-    users = relationship(Users)
-class Media(Base):
-    __tablename__='media'
-    id = Column(Integer, primary_key = True)
-    type = Column(String)
-    url = Column(String)
-    post_id = Column(Integer, ForeignKey('post.id'))
-    users = relationship(Users)
-    def to_dict(self):
-        return {}
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+    fecha_de_subscripcion = Column(DateTime, nullable=False)
+    email = Column(String(100), nullable=False)
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    usuario = relationship(Usuario)
+    planeta_id = Column(Integer, ForeignKey('planetas.id_planeta'))
+    planeta = relationship("Planetas")
+    vehiculo_id = Column(Integer, ForeignKey('vehiculos.id_vehiculos'))
+    vehiculo = relationship("Vehiculos")
+    nombre_id = Column(Integer, ForeignKey('personas.id_persona'))
+    nombre = relationship("Nombres")
+
+class Planetas(Base):
+    __tablename__ = 'planetas'  
+    id_planeta = Column(Integer, primary_key=True)
+    nombre_planeta = Column(String(100), nullable=False)
+    periodo_rotacion = Column(Integer, nullable=False)
+    diametro = Column(Float, nullable=False)
+    clima = Column(String(50), nullable=False)
+    terreno = Column(String(50), nullable=False)
+
+class Nombres(Base):
+    __tablename__ = 'personas'
+    id_persona = Column(Integer, primary_key=True)
+    nombre_persona = Column(String(100), nullable=False)
+    peso = Column(Integer, nullable=False)
+    color_de_piel = Column(String(20), nullable=False)
+    color_de_pelo = Column(String(20))
+    genero = Column(String(20))
+    birth_year = Column(Integer, nullable=False)
+
+class Vehiculos(Base):
+     __tablename__ = 'vehiculos'
+     id_vehiculos = Column(Integer, primary_key=True)
+     nombre_vehiculos = Column(String(100), nullable=False)
+     modelo = Column(String(50), nullable=False)
+     longitud = Column(Float, nullable=False)     
+     tripulacion = Column(Integer, nullable=False)
+     clase_de_vehiculo =Column(String, nullable=False)
+     capacidad_de_carga =Column(Integer, nullable=False)
+     velocidad_maxima =Column(Integer, nullable=False)
+
+def to_dict(self):
+    return {}
+
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
